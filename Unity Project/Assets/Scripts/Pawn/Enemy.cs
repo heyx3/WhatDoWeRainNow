34161fd0f3e9a84f4afd57f590e9b0e03c40a1f4
heyx3 <manning.w@husky.neu.edu>
2015-01-24 17:48:07 -0500

    Forgot to commit some files

---
+++ Unity Project/Assets/Scripts/Pawn/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A computer-controlled enemy with some kind of behavior each turn.
+/// </summary>
+public abstract class Enemy : Pawn
+{
+	public List<Vector2> PathAlongFloor = new List<Vector2>();
+
+	public float MinPathingUpdateTime = 3.0f,
+				 MaxPathingUpdateTime = 6.0f;
+
+
+	void Start()
+	{
+
+	}
+	protected virtual void Update()
+	{
+		base.Update();
+
+		//Follow the path.
+		Vector3 pos = MyTransform.position;
+		Vector2 toTarget = new Vector3(PathAlongFloor[0].x - pos.x, 0.0f,
+									   PathAlongFloor[0].y - pos.z);
+	}
+
+	private IEnumerator UpdatePathingCoroutine()
+	{
+		yield return new WaitForSeconds(UnityEngine.Random.Range(MinPathingUpdateTime,
+																 MaxPathingUpdateTime));
+
+		UpdatePath();
+
+		StartCoroutine(UpdatePathingCoroutine());
+	}
+
+	private void UpdatePath()
+	{
+
+	}
+}
---
+++ Unity Project/Assets/Scripts/Pawn/Pawn.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+/// <summary>
+/// An entity that takes actions during his turn.
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public abstract class Pawn : MonoBehaviour
+{
+	public enum WeaponTypes
+	{
+		Rock,
+		Paper,
+		Scissors,
+	}
+	public static Dictionary<WeaponTypes, WeaponTypes> Strengths = new Dictionary<WeaponTypes, WeaponTypes>()
+	{
+		{ WeaponTypes.Paper, WeaponTypes.Rock },
+		{ WeaponTypes.Rock, WeaponTypes.Scissors },
+		{ WeaponTypes.Scissors, WeaponTypes.Paper },
+	};
+	public static Dictionary<WeaponTypes, WeaponTypes> Weaknesses = new Dictionary<WeaponTypes, WeaponTypes>()
+	{
+		{ WeaponTypes.Paper, WeaponTypes.Scissors },
+		{ WeaponTypes.Rock, WeaponTypes.Paper },
+		{ WeaponTypes.Scissors, WeaponTypes.Rock },
+	};
+
+
+	public Transform MyTransform { get; private set; }
+	public CharacterController Controller { get; private set; }
+
+	/// <summary>
+	/// The amount of time left until this pawn can attack again.
+	/// </summary>
+	public float TimeLeftTillRecharged { get; set; }
+
+
+	public float RockRechargeTime = 0.5f,
+				 PaperRechargeTime = 1.0f,
+				 ScissorsRechargeTime = 2.0f;
+	public float MoveSpeed = 0.5f;
+
+
+	private float height = float.NaN;
+
+
+	protected virtual void Awake()
+	{
+		MyTransform = transform;
+		Controller = GetComponent<CharacterController>();
+	}
+	protected virtual void Update()
+	{
+		TimeLeftTillRecharged -= Time.deltaTime;
+	}
+	protected virtual void LateUpdate()
+	{
+		if (float.IsNaN(height))
+		{
+			if (Controller.enabled)
+				height = MyTransform.position.y + 0.01f;
+		}
+		else
+		{
+			Vector3 pos = MyTransform.position;
+			MyTransform.position = new Vector3(pos.x, height, pos.z);
+		}
+	}
+
+	protected void Attack(WeaponTypes weapon)
+	{
+		if (TimeLeftTillRecharged > 0.0f)
+			return;
+
+		switch (weapon)
+		{
+			case WeaponTypes.Rock:
+				TimeLeftTillRecharged = RockRechargeTime;
+				break;
+
+			case WeaponTypes.Paper:
+				TimeLeftTillRecharged = PaperRechargeTime;
+				break;
+
+			case WeaponTypes.Scissors:
+				TimeLeftTillRecharged = ScissorsRechargeTime;
+				break;
+
+			default: throw new NotImplementedException();
+		}
+	}
+}
