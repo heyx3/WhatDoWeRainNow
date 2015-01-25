739e4883cdcb0f92ced6d649c16d7e05680b538e
heyx3 <manning.w@husky.neu.edu>
2015-01-24 19:27:23 -0500

    Updated enemy logic

--- Unity Project/Assets/Scripts/Pawn/Enemy.cs
+++ Unity Project/Assets/Scripts/Pawn/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -13,20 +14,66 @@ public abstract class Enemy : Pawn
 
 	public float MinPathingUpdateTime = 3.0f,
 				 MaxPathingUpdateTime = 6.0f;
+	public float MovementSpeed = 0.05f;
 
 
 	void Start()
 	{
-
+		UpdatePath();
+		StartCoroutine(UpdatePathingCoroutine());
 	}
 	protected virtual void Update()
 	{
 		base.Update();
-
-		//Follow the path.
+		
 		Vector3 pos = MyTransform.position;
-		Vector2 toTarget = new Vector3(PathAlongFloor[0].x - pos.x, 0.0f,
-									   PathAlongFloor[0].y - pos.z);
+
+		//If the path is finished, calculate a new path.
+		if (PathAlongFloor.Count == 0)
+		{
+			UpdatePath();
+		}
+		//Otherwise, keep following the path.
+		else
+		{
+			Vector2 toTarget = new Vector3(PathAlongFloor[0].x - pos.x, 0.0f,
+										   PathAlongFloor[0].y - pos.z);
+
+			//If this enemy is close enough to the target, just snap to it.
+			float distSqr = toTarget.sqrMagnitude;
+			if (distSqr < (MovementSpeed * MovementSpeed))
+			{
+				MyTransform.position = new Vector3(PathAlongFloor[0].x, pos.y, PathAlongFloor[0].y);
+				PathAlongFloor.RemoveAt(0);
+			}
+			//Otherwise, keep moving like normal.
+			else
+			{
+				toTarget = toTarget.normalized * MovementSpeed * Time.deltaTime;;
+				Controller.Move(new Vector3(toTarget.x, 0.0f, toTarget.y));
+			}
+		}
+
+		//Constantly attack the enemy when possible.
+		Vector3 playerPos = Player.Instance.MyTransform.position;
+		Vector2 toPlayer = new Vector2(playerPos.x - pos.x, playerPos.z - pos.z);
+		float dist = toPlayer.magnitude;
+		if (dist < GameConstants.RockAttackDist)
+		{
+			Attack(WeaponTypes.Rock);
+		}
+		else
+		{
+			float ratio = Mathf.Abs(toPlayer.y / toPlayer.x);
+			if (ratio > 2.0f || ratio < 0.5f)
+			{
+				Attack(WeaponTypes.Paper);
+			}
+			else
+			{
+				Attack(WeaponTypes.Scissors);
+			}
+		}
 	}
 
 	private IEnumerator UpdatePathingCoroutine()
@@ -41,6 +88,15 @@ public abstract class Enemy : Pawn
 
 	private void UpdatePath()
 	{
-
+		//Choose a random destination.
+		List<mVector2i> freeSpaces = GameRegion.Instance.RoomObj.FreeSpaces;
+		mVector2i start = GameRegion.Instance.RoomObj.PosToRoomCoord(MyTransform.position),
+				  end = freeSpaces[UnityEngine.Random.Range(0, freeSpaces.Count)];
+		PathAlongFloor = GameRegion.Instance.RoomObj.FindPath(start, end, false).Select(
+							v2i =>
+							{
+								Vector3 pos = GameRegion.Instance.RoomObj.RoomCoordToPos(v2i);
+								return new Vector2(pos.x, pos.z);
+							}).ToList();
 	}
 }
